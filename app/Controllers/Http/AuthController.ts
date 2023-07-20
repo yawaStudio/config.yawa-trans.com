9bff866b292e3a79292e477400f7ef6de3b95160
Yawa Studio <128216695+yawaStudio@users.noreply.github.com>
2023-07-20 09:46:17 +0000

    auth errors fix

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -4,17 +4,17 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class AuthController {
     public async login({request, response, auth, session}: HttpContextContract) {
         const {email, password} = await request.all()
-    
+        console.log('email ', email )
         try {
-            await auth.attempt(email, password);
-
+            await auth.use('web').attempt(email, password)
         } catch (error) {
           
-          
+            console.log('error ',  error) 
             session.flash('login', 'Oups! Identifiants incorrects.')
         }
 
 
+            console.log('auth ',  auth.use('web').isLoggedIn)
         return response.redirect("/");
       }
    
